5db365b327147339faf46b273adb697a961b06f3
Rleaf <calcifer44@gmail.com>
2022-09-20 12:45:05 -0400

    attention: flesh out attn

--- model/transformer.py
+++ model/transformer.py
@@ -1,63 +1,78 @@
 import torch
 import torch.nn as nn
 
-"""
-Can standardly create the attn mechanism presented w/ what's below in the comments
-however I'd like to try and do a more efficient operation where operations are done
-in batches.
-
-enc(a)=b  ->  codebook(b)=c  ->  transformer(c)=seq  ->  dec(seq)
-"""
-
 class Attention(nn.Module):
    """
-   qkv(x)=x2  ->  preprocess(x2)=q,k,v  ->  sdp(q, k, v)=out
+   Giving it a go in batches
+
+   qkv(x)=x2  ->  preprocess(x2)=q,k,v  ->
+   sdp(q, k, v)=out  ->  self.out(out)=out
 
    preprocess() {
       - aliquot tensor x2 to segregate q, k, and v into a list and store in qkv
       - iterate through the list and isolate both head & head_dim in each element and
          the q, k, and v from each other
-      - return q, k, v represented as 4d tensors composed (batch, head, inp_dim[1], head_dim)
+      - return q, k, v represented as 4d tensors composed (batch, inp_dim[1], head, head_dim)
    }
    """
-   def __init__(self, dim, head_dim, heads):
+   def __init__(self, dim, head_dim, heads, dropout = 0.0):
       super().__init__()
+      inner_dim = head_dim * heads
       self.heads = heads
-      self.inner_dim = head_dim * heads
-      self.qkv = nn.Linear(dim, self.inner_dim * 3, bias = False) # (B, N, M) -> (B, N, D)
+      self.qkv = nn.Linear(dim, inner_dim * 3, bias = False) # (B, N, M) -> (B, N, D)
+      self.out = nn.Sequential(
+         nn.Linear(inner_dim, dim),
+         nn.Dropout(dropout)
+      )
 
    def forward(self, x, mask = None): # (B, N, M)
       qkv = self.qkv(x)  # (B, N, D)
       qkv = qkv.chunk(3, dim = -1) # ((B, N, D/3) * 3)
       b, n, d = qkv[0].shape # (B, N, inner_dim)
 
-      q, k, v = [qkv[i].reshape(b, n, self.heads, d // self.heads).permute(0, 2, 1, 3) \
-          for i in range(len(qkv))] # (B, N, H, head_dim) -> (B, H, N, head_dim)
-      attn = torch.matmul(q, k.permute(0, 1, 3, 2)) # (B, H, N, N)
+      q, k, v = [qkv[i].reshape(b, n, self.heads, d // self.heads) \
+         for i in range(len(qkv))] # (B, N, H * head_dim) -> (B, N, H, head_dim)
+      attn = torch.matmul(q, k.permute(0, 1, 3, 2)) # (B, N, H, H)
       attn = attn / q.shape[-1] ** (1/2)
 
       if mask:
          pass
 
       attn_softmax = attn.softmax(dim = -1)
-      out = torch.matmul(attn_softmax, v)
+      out = torch.matmul(attn_softmax, v) # (B, N, H, head_dim)
+      out = out.reshape(b, n, -1) # (B, N, H * head_dim)
+      out = self.out(out) # (B, N, inner_dim) -> (B, N, M)
       return out 
 
 torch.manual_seed(0)
-x = torch.rand((2, 3, 4))
 attn = Attention(4, 6, 8)
+x = torch.rand((2, 3, 4))
 out = attn(x)
 print('out', out.shape)
 
-class Decoder(nn.Module):
+# class Decoder(nn.Module):
+#    def __init__(self,
+#       num_heads,
+#       emb_dim,
+#       ff_dim,
+#       dropout,
+#       num_dec) -> None:
+#       super().__init__()
+
+#       def forward(self, x):
+#          pass
+
+class Transformer(nn.Module):
    def __init__(self,
-      num_heads,
-      emb_dim,
+      dim,
+      heads,
       ff_dim,
       dropout,
-      num_dec) -> None:
+      num_dec_layers) -> None:
       super().__init__()
       """
+      enc(a) = b -> codebook(b) = c -> transformer(c) = seq -> dec(seq)
+
       (vanilla dec)
       mask_attn(a)=b  ->  res_norm(b)=b2  ->  cross_attn(enc,b2)=c  ->
       res_norm(c)=c2  ->  ffn(c2)=d  ->  res_norm(d)=d2  ->  Linear(d2)=e  ->
@@ -66,20 +81,8 @@ class Decoder(nn.Module):
       (sparse transformer: https://arxiv.org/pdf/1904.10509.pdf)
       this may take a bit (9/19/22)
       """
-
-      def forward(self, x):
-         pass
-
-class Transformer(nn.Module):
-   def __init__(self,
-      num_heads,
-      emb_dim,
-      ff_dim,
-      dropout,
-      num_dec_layers) -> None:
-      super().__init__()
-
-      self.decoder = Decoder(num_heads, emb_dim, ff_dim, dropout, num_dec_layers)
+      self.layers = nn.ModuleList([])
+      # self.decoder = Decoder(num_heads, emb_dim, ff_dim, dropout, num_dec_layers)
 
       def forward(self, x):
          pass
