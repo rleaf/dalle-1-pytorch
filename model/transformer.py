2f394031f876a475b5a0f2ad53bcf982de2840c6
Rleaf <calcifer44@gmail.com>
2022-09-21 00:29:32 -0400

    transformer: build transformer. work on masking

--- model/transformer.py
+++ model/transformer.py
@@ -17,6 +17,7 @@ class Attention(nn.Module):
    """
    def __init__(self, dim, head_dim, heads, dropout = 0.0):
       super().__init__()
+
       inner_dim = head_dim * heads
       self.heads = heads
       self.qkv = nn.Linear(dim, inner_dim * 3, bias = False) # (B, N, M) -> (B, N, D)
@@ -42,51 +43,73 @@ class Attention(nn.Module):
       out = torch.matmul(attn_softmax, v) # (B, N, H, head_dim)
       out = out.reshape(b, n, -1) # (B, N, H * head_dim)
       out = self.out(out) # (B, N, inner_dim) -> (B, N, M)
-      return out 
+      return out
 
-torch.manual_seed(0)
-attn = Attention(4, 6, 8)
-x = torch.rand((2, 3, 4))
-out = attn(x)
-print('out', out.shape)
+# torch.manual_seed(0)
+# attn = Attention(4, 6, 8)
+# x = torch.rand((2, 3, 4))
+# out = attn(x)
+# print('out', out.shape)
 
-# class Decoder(nn.Module):
-#    def __init__(self,
-#       num_heads,
-#       emb_dim,
-#       ff_dim,
-#       dropout,
-#       num_dec) -> None:
-#       super().__init__()
+class FeedForward(nn.Module):
+   def __init__(self, dim, ff_dim, dropout):
+      super().__init__()
 
-#       def forward(self, x):
-#          pass
+      self.mlp = nn.Sequential(
+         nn.Linear(dim, ff_dim),
+         nn.ReLU(),
+         nn.Dropout(dropout),
+         nn.Linear(ff_dim, dim),
+         nn.LayerNorm(dim)
+      )
+   
+   def forward(self, x):
+      return self.mlp(x)
 
 class Transformer(nn.Module):
+   """
+   (vanilla dec)
+   mask_attn(a)=b  ->  res_norm(b)=b2  ->  cross_attn(enc,b2)=c  ->
+   res_norm(c)=c2  ->  ffn(c2)=d  ->  res_norm(d)=d2  ->  Linear(d2)=e  ->
+   softmax(e)=out
+
+   (sparse transformer: https://arxiv.org/pdf/1904.10509.pdf)
+   this may take a bit (9/19/22)
+   """
    def __init__(self,
       dim,
+      depth,
+      head_dim,
       heads,
       ff_dim,
-      dropout,
-      num_dec_layers) -> None:
+      dropout = 0.0,
+      ff_dropout = 0.0):
       super().__init__()
+
+      self.layers = nn.ModuleList([])
+
+      for _ in range(depth):
+         self.layers.append(nn.ModuleList([
+            Attention(dim = dim, head_dim = head_dim, heads = heads, dropout = dropout),
+            FeedForward(dim = dim, ff_dim = ff_dim, dropout = ff_dropout)
+         ]))
+
+   def forward(self, x):
       """
       enc(a) = b -> codebook(b) = c -> transformer(c) = seq -> dec(seq)
-
-      (vanilla dec)
-      mask_attn(a)=b  ->  res_norm(b)=b2  ->  cross_attn(enc,b2)=c  ->
-      res_norm(c)=c2  ->  ffn(c2)=d  ->  res_norm(d)=d2  ->  Linear(d2)=e  ->
-      softmax(e)=out
-
-      (sparse transformer: https://arxiv.org/pdf/1904.10509.pdf)
-      this may take a bit (9/19/22)
       """
-      self.layers = nn.ModuleList([])
-      # self.decoder = Decoder(num_heads, emb_dim, ff_dim, dropout, num_dec_layers)
+      for attn, ff in self.layers:
+         x = attn(x) + x
+         x = ff(x)
 
-      def forward(self, x):
-         pass
+      return x
 
+
+torch.manual_seed(0)
+model = Transformer(4, 3, 6, 8, 4)
+x = torch.rand((2, 3, 4))
+out = model(x)
+print('out', out.shape)
 
 
 # def sdp(q, k, v):
