82c8920d191d4101c8606717e06db0f82ce38487
Rleaf <calcifer44@gmail.com>
2023-03-20 17:04:45 -0400

    template

---
+++ train_dalle.py
@@ -0,0 +1,34 @@
+import code
+import os
+import argparse
+import math
+
+# wandb
+import wandb
+
+# torch
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+import torchvision.datasets as dset
+import torchvision.transforms as T
+
+# matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+# files
+from model.dvae import dVAE
+
+def main(config):
+   pass
+
+if __name__ == '__main__':
+   args = argparse.ArgumentParser()
+
+   args.add_argument('--epochs', type=int, default = 10)
+
+   config = args.parse_args()
+
+   main(config)
